d650e2644befd1765441b451002687f7bdfdefe2
akubota95 <akubota@g.hmc.edu>
2025-09-14 20:15:53 -0700

    Revise copyright and format file description

--- data_review_page.py
+++ data_review_page.py
@@ -8,7 +8,19 @@
 #
 # File Description:
 # This file contains the code for the data review page, which loads data from a results file,
-# displays it in a table, and provides options to export the data, show statistics, and generate graphs.
+# displays it in a table, and provides options to export the data, show statistics, and generate 
+# graphs.
+#
+# Copyright 2025 SFSU PHAST Lab
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 ###########################################################################################################
 
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget,
