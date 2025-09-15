514ef7f5760fd52a1883fd74921aee378b1eaac7
akubota95 <akubota@g.hmc.edu>
2025-09-14 20:13:47 -0700

    Add copyright and license information to main_page.py

--- main_page.py
+++ main_page.py
@@ -16,6 +16,19 @@
 # This file contains the code for the main window of the application and initializes 
 # the main menu, linking it to the calibration, data review, and image editing pages.
 #
+# Copyright 2025 SFSU PHAST Lab
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 ############################################################################################
 
 import sys
