d34a5f45596a23c26b8b760ba501fedb7f3fcc1a
akubota95 <akubota@g.hmc.edu>
2025-09-14 20:18:53 -0700

    Update header

--- main_page.py
+++ main_page.py
@@ -1,5 +1,5 @@
 ############################################################################################
-# Project Name: Motor Skill Acquisition Error Management System 
+# Project Name: PinPointer
 # 
 # Filename: main_page.py
 # 
@@ -17,13 +17,10 @@
 # the main menu, linking it to the calibration, data review, and image editing pages.
 #
 # Copyright 2025 SFSU PHAST Lab
-#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-#
 #        http://www.apache.org/licenses/LICENSE-2.0
-#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
